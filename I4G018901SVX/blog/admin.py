8c75ccd60b9cb38b08d3cc20c769abf7578dbcfc
akinlope <akinrimisitt@gmail.com>
2022-06-22 14:00:57 +0100

    almost completed

---
+++ I4G018901SVX/blog/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from blog.models import Post
+
+# Register your models here.
+admin.site.register(Post)
+# admin.site.register(slug)
+# admin.site.register(author)
+# admin.site.register(body)
+# admin.site.register(publish)
+# admin.site.register(created)
+# admin.site.register(updated)
+# admin.site.register(status)
